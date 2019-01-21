9c2c0218143aeef85b6ba47403031c1b8063b55c
NFS101 <realnfs101@gmail.com>
2019-01-21 14:40:54 +0600

    Update

--- src/Player.py
+++ src/Player.py
@@ -1,4 +1,4 @@
-import curses
+import curses, keyword
 from CreateMap import CreateMap
 
 class Player:
@@ -21,7 +21,7 @@ class Player:
         while self.__IsRunning:
             InputKey = self.__stdscr.getch()
 
-            if InputKey == ord("w") or InputKey == ord("W") or InputKey == 72:
+            if InputKey == ord("w") or InputKey == ord("W") or InputKey == 0:
                 self.__PositionY -= 1
 
                 if self.__PositionY == 0:
@@ -31,7 +31,7 @@ class Player:
                 self.__ObjectMap.create_map()
                 self.__print_player()
 
-            if InputKey == ord("s") or InputKey == ord("S") or InputKey == 80:
+            if InputKey == ord("s") or InputKey == ord("S"):
                 self.__PositionY += 1
 
                 if self.__PositionY == 9:
@@ -41,7 +41,7 @@ class Player:
                 self.__ObjectMap.create_map()
                 self.__print_player()
 
-            if InputKey == ord("a") or InputKey == ord("A") or InputKey == 75:
+            if InputKey == ord("a") or InputKey == ord("A"):
                 self.__PositionX -= 1
 
                 if self.__PositionX == 0:
@@ -51,7 +51,7 @@ class Player:
                 self.__ObjectMap.create_map()
                 self.__print_player()
 
-            if InputKey == ord("d") or InputKey == ord("D") or InputKey == 77:
+            if InputKey == ord("d") or InputKey == ord("D"):
                 self.__PositionX += 1
                 
                 if self.__PositionX == 19:
@@ -63,4 +63,6 @@ class Player:
 
             if InputKey == 27:
                 self.__IsRunning = False
-                
+            
+            else:
+                curses.noecho()
